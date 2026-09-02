9d2c20e01683467193648efb8721f613d9910ba7
bongsuk0708 <bong0708@daum.net>
2026-09-02 13:04:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE = 'undong-v3';
+var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES).catch(function(){}); }));
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.map(function(k){ if(k!==CACHE) return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  var u = e.request.url;
+  if(u.indexOf('firebaseio') > -1 || u.indexOf('firebasedatabase') > -1) return;
+  e.respondWith(
+    fetch(e.request).then(function(r){
+      var copy = r.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+      return r;
+    }).catch(function(){ return caches.match(e.request); })
+  );
+});
