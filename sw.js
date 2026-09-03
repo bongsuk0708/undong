3068d7fdb3e80e381192605a86951414f68a4ac2
bongsuk0708 <bong0708@daum.net>
2026-09-03 09:46:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'undong-v6';
+var CACHE = 'undong-v8';
 var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
